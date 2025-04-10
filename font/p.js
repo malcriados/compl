75bc9f6599521d2a934f55675e9fa20cb0e2f2d8
10 <edyjald@gmail.com>
2025-04-10 08:05:20 -0500

    v11
    v11

---
+++ font/p.js
@@ -0,0 +1,11 @@
+function fun() {
+    let inputPswd = document.getElementById("validate").value;
+    let correctPswd = "liderprimero";
+    if (inputPswd === correctPswd) {
+      document.getElementById("fds").style.display = window.location.href =
+        "/1ro968x3848i9498ii5i68x9downx.html";
+      document.getElementById("pswd").style.display = "none";
+    } else {
+      alert("Contraseña Incorrecta");
+    }
+  }
---
+++ font/s.js
@@ -0,0 +1,11 @@
+function fun() {
+    let inputPswd = document.getElementById("validate").value;
+    let correctPswd = "segundolider";
+    if (inputPswd === correctPswd) {
+      document.getElementById("fds").style.display = window.location.href =
+        "/2do968x3848i9498ii5i68x9downx.html";
+      document.getElementById("pswd").style.display = "none";
+    } else {
+      alert("Contraseña Incorrecta");
+    }
+  }
