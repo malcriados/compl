6cb5e8b23ed44f1b6266d48bc055b77e85f8e17e
10 <edyjald@gmail.com>
2025-04-14 04:23:24 -0500

    v16
    v16

--- font/sec/s1.js
+++ font/sec/s1.js
@@ -1,6 +1,6 @@
 function fun() {
     let inputPswd = document.getElementById("validate").value;
-    let correctPswd = "1";
+    let correctPswd = "primerolider";
     if (inputPswd === correctPswd) {
       document.getElementById("fds").style.display = window.location.href =
         "/1ro968x3848i9498ii5i68x9downx.html";
