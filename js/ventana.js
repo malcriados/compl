b536743a6ca96a4b041aabc80c6b49f9313962d8
10 <edyjald@gmail.com>
2025-05-09 19:32:49 -0500

    ll
    ll

---
+++ js/ventana.js
@@ -0,0 +1,7 @@
+function cerrarModal1() {
+    document.getElementById("modalOverlay1").style.display = "none";
+  }
+
+  window.onload = function () {
+    document.getElementById("modalOverlay1").style.display = "flex";
+  };
